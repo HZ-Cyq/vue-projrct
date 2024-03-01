2f51708001772caafa8f677bde3c152bedbcfbb6
HZ-Cyq <1369509439@qq.com>
2024-03-01 11:35:15 +0800

    vuex测试暂提

---
+++ src/Vuex.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        111
+    </div>
+    <!-- <div>
+        使用mutation触发改变状态{{$main.state.count}}
+    </div> -->
+</template>
+<!-- <script>
+    import {mapMutations, mapActions, mapGetters} from "vuex";
+</script> -->
+<script>
+  export default {
+    data() {
+      return {
+        text:'千里之行，始于足下',
+      }
+    },
+    // methods: {
+    //     ...mapMutations([
+    //         'jian',
+    //     ])
+    // }
+  }
+</script>
--- src/main.js
+++ src/main.js
@@ -2,10 +2,18 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
+import Vuex from './Vuex.vue'
 
-let app = createApp(App);
+// let state = {
+//     count: 1;
+// }
+// let mutations = {
+//     jian(state) {
+//         state.count--;
+//     }
+//     jia(state, n) {
+//         state.count+=n.a;
+//     }
+// }
+let app = createApp(Vuex);
 let vm = app.mount("#app");
-console.log("before log vm");
-console.log(vm);
-console.log("after log vm");
-console.log("app.text: " + app._component.data().text);
