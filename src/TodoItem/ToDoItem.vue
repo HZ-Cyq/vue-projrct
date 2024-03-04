134ac07c9b38959b286a3c894782ae3c5b1c3197
HZ-Cyq <1369509439@qq.com>
2024-03-04 15:25:41 +0800

    给Todos一个唯一的ID

--- src/TodoItem/ToDoItem.vue
+++ src/TodoItem/ToDoItem.vue
@@ -1,21 +1,27 @@
 <template>
     <div>
-       <input type="checkbox" id="todo-item" checked="false"/>
-       <label for="todo-item">{{label}}</label> 
+       <input type="checkbox" :id="id" :checked="isDone"/>
+       <label :for="id">{{label}}</label> 
+       <!-- <input type="checkbox" id="todo-item" :checked="isDone"/> -->
+       <!-- <label :for="label">dwq</label> -->
     </div>
 </template>
 <script>
+import uniqueId from "lodash.uniqueid";
     export default {
-        data() {
-            return {
-                "text": "千里之行，始于足下",
-            };
-        },
+        // 使用props让组件动态化，prop的值给予了组件影响其显示的初始状态
         props: {
-            // required为true:相当于告诉Vue,我们希望每个该组件的实例都必须有个label字段，如果ToDoItem组件没有label字段，Vue会提示警告。【只是提示警告，不会不显示】
+            // required为true:相当于告诉Vue,我们希望每个该组件的truetrue须有个label字段，如果ToDoItem组件没有label字段，Vue会提示警告。【只是提示警告，不会不显示】
             label: {required: true, type: String},
             done: {default: false, type: Boolean},
             count: {type: Number},
-        }
+        },
+        // Vue的数据对象，在应用程序中，我们没有记录todoitem是否已完成，使用data可以记录该状态。
+        data() {
+            return {
+                isDone: this.done,
+                id: uniqueId("todo-"),
+            };
+        },
     };
 </script>
