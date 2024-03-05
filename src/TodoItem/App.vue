06c248731ec1486294e315f815df1c1a624298f6
HZ-Cyq <1369509439@qq.com>
2024-03-05 15:49:41 +0800

    使用 Vue event、method 和 model 添加一个新的 todo 表单

--- src/TodoItem/App.vue
+++ src/TodoItem/App.vue
@@ -1,6 +1,7 @@
 <template>
     <div id = "app">
         <h1>To-Do List</h1>
+        <to-do-form @todo-added="addToDo"></to-do-form>
         <ul>
             <li v-for="item in ToDoItems" :key="item.id">
                 <!-- <to-do-item label="My ToDo Item" :done="true"></to-do-item> -->
@@ -11,11 +12,12 @@
 </template>
 <script>
     import ToDoItem from "./ToDoItem.vue";
+    import ToDoForm from "./ToDoForm.vue";
     import uniqueId from "lodash.uniqueid";
     export default {
         name: "app",
         components: {
-            ToDoItem,
+            ToDoItem, ToDoForm,
         },
         data() {
             return {
@@ -24,8 +26,13 @@
                     { id: uniqueId("todo-"), label: "Create a Vue project with the CLI",done: true},
                     { id: uniqueId("todo-"), label: "Have fun", done: true },
                     { id: uniqueId("todo-"), label: "Create a to-do list", done: false },
-      ],
-    };
-  },
+                  ],
+            };
+        },
+        methods: {
+            addToDo(toDoLabel) {
+                this.ToDoItems.push({id:uniqueId("todo-"), label:toDoLabel, done:false});
+            },
+        },
 };
 </script>
---
+++ src/VModel/App.vue
@@ -0,0 +1,15 @@
+<template>
+    <div id="app">
+        <input text="text" v-model.lazy="message">
+        <p>您输入的内容是：{{ message }}</p>
+    </div>
+</template>
+<script>
+    export default {
+        data() {
+            return {
+                message:"",
+            }
+        },
+    }
+</script>
